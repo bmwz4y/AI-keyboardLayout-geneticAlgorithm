365987cb31ad2e96fff6bded89f15043bab3e522
YEH <bmwz4_y@hotmail.com>
2019-01-09 18:00:18 +0200

    added the app code (VS solution folder)

---
+++ GeneticKeyboard1/GeneticKeyboard1/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GeneticKeyboard1
+{
+    public partial class Form1 : Form
+    {
+        private string startButtonText = "Start computing";
+        private string finishButtonText = "Finish computing now";
+        private string stopwatchLabelText = "Total time of last run:";
+        private string stopwatchInitialText = "hh:mm:ss.ttttttt";
+        private string startTimeLabelText = "current run started at:";
+        private string startTimeInitialText = "hh:mm:ss";
+        private ThreadStart _childref;
+        private Thread _childThread;
+
+        // Create new stopwatch.
+        private readonly Stopwatch _stopwatch = new Stopwatch();
+
+        //string builder to help in formating output
+        private readonly StringBuilder _stringBuilder = new StringBuilder();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            button1.Text = startButtonText;
+            button2.Text = finishButtonText;
+            label1.Text = stopwatchLabelText.ToUpper();
+            label2.Text = stopwatchInitialText.ToUpper();
+            label3.Text = startTimeLabelText.ToUpper();
+            label4.Text = startTimeInitialText.ToUpper();
+
+            //before first run only start button can be pressed and the result layout isn't visible at all and make it readonly
+            button1.Enabled = true;
+            button2.Enabled = false;
+            richTextBox1.ReadOnly = true;
+            label3.Visible = false;
+            label4.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //switch button off and turn the other on
+            button1.Enabled = false;
+            button2.Enabled = true;
+
+            //disable the Layout richtext if it is visible on consecutive runs and others
+            richTextBox1.Enabled = false;
+            label1.Enabled = false;
+            label2.Enabled = false;
+            label3.Enabled = true;
+            label4.Enabled = true;
+
+            //set start time as now
+            label4.Text = DateTime.Now.ToShortTimeString();
+
+            //show start time
+            label3.Visible = true;
+            label4.Visible = true;
+
+            // Begin timing.
+            _stopwatch.Start();
+
+            //call child thread to start solving without hanging the main program
+            _childref = SolveThread;
+            _childThread = new Thread(_childref);
+            _childThread.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //switch button off and turn the other on
+            button1.Enabled = true;
+            button2.Enabled = false;
+
+            //stop computing
+            _childThread.Abort();
+
+            //format and show output
+            _stringBuilder.Clear();
+            for (int i = 0; i < KeyboardGene.LayoutRowCount; i++)
+            {
+                _stringBuilder.Append("I  ");
+                for (int j = 0; j < KeyboardGene.LayoutColumnCount; j++)
+                {
+                    _stringBuilder.Append(KeyboardGene.ResultLayoutMatrix.GetValue(i, j));
+                    //here i used the I english character with spaces between each to keep it left to right so key would be in correct position
+                    _stringBuilder.Append("  I  ");
+                }
+                _stringBuilder.AppendLine();
+            }
+            _stringBuilder.AppendLine();
+            _stringBuilder.Append("The above layout has a fitness value: " + KeyboardGene.MinFitness);
+            richTextBox1.Text = _stringBuilder.ToString();
+
+            //show the Layout richtext by using visible and enabled to be able to copy
+            richTextBox1.Enabled = true;
+            label1.Enabled = true;
+            label2.Enabled = true;
+            label3.Enabled = false;
+            label4.Enabled = false;
+
+            // Stop timing.
+            _stopwatch.Stop();
+
+            //show elapsed time since it ran and clear stopwatch
+            label2.Text = _stopwatch.Elapsed.ToString("c");
+            _stopwatch.Reset();
+
+            //hide start time
+            label3.Visible = false;
+            label4.Visible = false;
+        }
+
+        private static void SolveThread()
+        {
+            //I'm basically using the current used keyboard layout and my constructed seed as both to be initial father and mother in every run
+            KeyboardGene fatherKeyboardGene = new KeyboardGene(KeyboardGene.SeedLayoutMatrix);
+            KeyboardGene motherKeyboardGene = new KeyboardGene(KeyboardGene.CurrentUsedLayoutMatrix);
+
+            //This is to keep result from being destroyed which will be used to hold best of each new generation
+            KeyboardGene resultKeyboardGene = new KeyboardGene(KeyboardGene.ResultLayoutMatrix);//initialize to default (alphabet)
+            resultKeyboardGene.ComputeFitness();//and compute default fitness (alphabet fitness value)
+            KeyboardGene.MinFitness = resultKeyboardGene.Fitness;
+
+            try
+            {
+                //to hold all created solutions dynamically
+                KeyboardGene[] population = new KeyboardGene[KeyboardGene.PopulationSize];
+
+                //add seeds
+                population[0] = fatherKeyboardGene;
+                population[1] = motherKeyboardGene;
+
+
+                //here is the start actual computing
+                //let's find what is the evaluation value for each
+                population[0].ComputeFitness();
+                population[1].ComputeFitness();
+
+                //start infinite loop stopped only when user calls stopping
+                while (true)
+                {
+                    //start at 2 because of father and mother
+                    for (int i = 2; i < KeyboardGene.PopulationSize; i += 2) //each loop creates a generation
+                    {
+                        //create 2 new children
+                        population[i] = new KeyboardGene(population[i - 2].layoutMatrix); //based on its father
+                        population[i + 1] = new KeyboardGene(population[i - 1].layoutMatrix); //based on its mother
+
+                        //both to cross over (taking half the keys from father and the other half from mother)
+                        //not implemented for now because it'll introduce conflicts and it'll be so time consuming
+                        //because for each child I need to loop once to copy and another to read values and make sure all keys are present and if not then copy missing into one placement of any of the duplicate keys
+                        //and if I really want cross over to be good I have to get fitness for each key individually and compare father to mother then take the better one
+
+                        //change some keys positions by mutation for both new entries
+                        population[i].Mutate();
+                        population[i + 1].Mutate();
+
+                        //let's find what is the evaluation value for each
+                        population[i].ComputeFitness();
+                        population[i + 1].ComputeFitness();
+                    }
+                    //find which of the population has minimum fitness (least amount of time which is best)
+                    foreach (var gene in population)
+                    {
+                        if (gene.Fitness < KeyboardGene.MinFitness)
+                        {
+                            resultKeyboardGene = gene;
+                            KeyboardGene.MinFitness = gene.Fitness;
+                        }
+                    }
+
+                    //change worst ancestor after first loop
+                    if (population[0].Fitness > population[1].Fitness && population[0].Fitness > resultKeyboardGene.Fitness)
+                    {
+                        population[0] = resultKeyboardGene;
+                    }
+                    else if (population[1].Fitness > population[0].Fitness && population[1].Fitness > resultKeyboardGene.Fitness)
+                    {
+                        population[1] = resultKeyboardGene;
+                    }
+                }//end of the infinite loop
+            }
+            finally
+            {
+                //here it saves the result in the static result matrix
+                KeyboardGene.ResultLayoutMatrix = resultKeyboardGene.layoutMatrix;
+            }
+        }
+
+    }
+}
---
+++ GeneticKeyboard1/GeneticKeyboard1/KeyboardGene.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Windows;
+
+namespace GeneticKeyboard1
+{
+    public class KeyboardGene
+    {
+
+        //variables to hold sizes of matrices and other constants
+        private const int N = 32; //total number of keys that have flow data (digraph probability) available
+        public const int LayoutRowCount = 4;//how many rows in the layout
+        public const int LayoutColumnCount = 10;//how many columns in the layout
+        //private const double Tolerance7 = 0.0000001;//tolerance level of any double comparision in this program
+        public const int PopulationSize = 100;//max number of saved genes at a time
+        private static readonly Random Random = new Random();//for mutations
+        private const int MaxMutations = 2;//max amount of mutations per creation of child will be 4 individual keys out of the total N but we are swapping so divide by 2
+        private static readonly double MutationRate = (double)MaxMutations / N;//to use when actually mutating
+
+        //from "ArabicDigraphsFlow.txt" which I extracted from a research paper but for my keyboard, using integers by multiplying original float by 10 to reduce computer instructions
+        // since I'm merging both 'أ' and 'إ' to the same key, I have to merge corresponding rows and columns by taking the max value of each and put on the first one (i.e. the 'أ')
+        private static readonly int[,] FlowMatrix = new int[N, N]{
+            { 0,12,7,10,11,6,8,3,7,6,0,4,7,4,2,5,4,3,0,3,2,2,0,2,0,1,1,1,2,1,1,0 }
+            , { 83,5,4,3,7,0,0,2,2,7,0,1,1,2,1,2,1,1,3,1,1,0,0,1,0,2,0,1,1,0,0,1 }
+            , { 3,9,1,5,4,6,8,3,6,2,0,6,2,4,13,2,2,3,1,2,1,1,0,1,0,1,2,1,1,1,1,2 }
+            , { 8,16,3,1,4,1,1,3,1,2,0,2,3,2,0,2,0,2,2,2,0,1,0,0,0,0,0,1,0,0,0,0 }
+            , { 2,4,3,4,0,2,3,3,2,1,0,3,2,2,1,2,2,1,3,1,1,0,0,1,0,0,1,0,0,0,0,0 }
+            , { 12,3,9,11,5,0,1,1,3,3,0,1,1,1,0,2,0,0,3,1,1,0,0,0,0,0,0,0,0,0,0,0 }
+            , { 8,2,6,3,5,0,0,3,4,3,0,2,1,1,2,2,2,2,1,1,1,2,0,1,0,1,0,1,0,0,1,0 }
+            , { 9,7,3,2,2,3,1,1,1,1,0,0,0,4,1,2,1,2,0,0,0,0,0,0,0,1,0,0,0,0,0,0 }
+            , { 4,4,2,1,2,1,3,2,0,2,0,0,0,2,0,1,1,0,1,1,1,0,0,1,0,0,0,0,0,0,0,0 }
+            , { 3,6,2,4,2,0,1,2,3,0,0,0,0,1,1,0,1,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0 }
+            , { 1,3,13,2,0,2,4,0,1,2,0,3,0,1,1,1,1,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0 }
+            , { 5,3,4,3,2,2,1,1,2,3,0,1,1,0,0,0,3,3,0,1,1,0,0,0,0,1,0,0,0,0,0,0 }
+            , { 2,4,3,1,1,3,1,3,2,1,0,1,0,1,1,0,0,0,1,1,0,1,0,0,0,0,0,0,1,0,0,0 }
+            , { 4,4,3,3,2,2,2,1,1,0,0,0,0,0,1,1,0,1,2,0,0,0,0,0,0,0,0,0,0,0,0,0 }
+            , { 2,3,1,0,2,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0 }
+            , { 2,5,2,1,1,0,2,1,1,0,0,0,0,1,0,0,0,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0 }
+            , { 2,4,2,1,2,1,1,2,1,1,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0 }
+            , { 2,4,1,2,1,0,1,2,1,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0 }
+            , { 0,6,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }
+            , { 2,3,1,2,2,1,2,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }
+            , { 1,2,0,1,1,1,0,1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0 }
+            , { 1,3,1,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }
+            , { 1,0,1,1,0,1,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0 }
+            , { 1,1,1,0,1,1,0,1,1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0 }
+            , { 0,7,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }
+            , { 1,2,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0 }
+            , { 1,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0 }
+            , { 1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }
+            , { 0,2,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }
+            , { 1,0,1,0,1,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }
+            , { 0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }
+            , { 3,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 } };
+
+        //from "ArabicLettersArangedByProbability.txt" the probability of each key which I also extracted from a research paper but for my keyboard
+        public static readonly char[] KeyArrangedByProbabilityArray = new char[N]{
+            'ا', 'ل', 'ي', 'م', 'و', 'ن', 'ر', 'ت', 'ب', 'ع', 'ة', 'د', 'ه', 'س', 'ف', 'ك', 'ق', 'ح', 'أ', 'ج', 'ص', 'ش', 'ط', 'ى', 'خ', 'ز', 'ث', 'ذ', 'ض', 'غ', 'ء' , 'ظ'};
+
+        //from "ArabicLetterProbability.txt" the probability of each key which I also extracted from a research paper but for my keyboard
+        private static readonly double[] KeyProbabilityArray = new double[N]{
+            14.8,11.4,8,6.4,5.6,5.1,4.7,4.1,3.8,3.4,3.3,3.1,2.7,2.6,2.5,2.1,2.1,1.8,2.6,1.5,1,1,1,0.9,0.9,0.6,0.6,0.6,0.5,0.4,0.3,0.2 };
+
+        //from "Seed1.txt" which I constructed in Graduation Project 1
+        public static readonly char[,] SeedLayoutMatrix = new char[LayoutRowCount, LayoutColumnCount]{
+            { 'ط', 'ة', 'ت', 'ي', 'ل', 'ا', 'و', 'ر', 'ه', 'b' }
+            , { 'ظ', 'س', 'ع', 'ن', 's', 's', 'م', 'ب', 'د', 'أ' }
+            , { '.', 'ش', 'ص', 'ء', 'ج', 'ح', 'ك', 'ق', 'ف', 'ؤ' }
+            , { 'g', '1', 'غ', 'ض', 'ث', 'ذ', 'خ', 'ى', 'ز', 'e' } };
+
+        //almost like the current most used layout in order to introduce little change (acceptance of reality)
+        public static readonly char[,] CurrentUsedLayoutMatrix = new char[LayoutRowCount, LayoutColumnCount]{
+            { 'ص', 'ث', 'ق', 'ف', 'غ', 'ع', 'ه', 'خ', 'ح', 'b' }
+            , { 'س', 'ي', 'ب', 'ل', 's', 's', 'ن', 'م', 'ك', 'ط' }
+            , { '.', 'ء', 'ؤ', 'ر', 'ى', 'ة', 'و', 'ز', 'ظ', 'د' }
+            , { 'g', '1', 'ض', 'ش', 'ذ', 'أ', 'ا', 'ت', 'ج', 'e' } };
+
+        //output matrix, but initialized with an alphabet order for testing purposes
+        public static char[,] ResultLayoutMatrix = new char[LayoutRowCount, LayoutColumnCount]{
+            { 'ح', 'ج', 'ث', 'ة', 'ت', 'ب', 'ء', 'أ', 'ا', 'b' }
+            , { 'ص', 'ش', 'س', 'ز', 's', 's', 'ر', 'ذ', 'د', 'خ' }
+            , { '.', 'ل', 'ك', 'ق', 'ف', 'غ', 'ع', 'ظ', 'ط', 'ض' }
+            , { 'g', '1', 'ي', 'ى', 'ؤ', 'و', 'ه', 'ن', 'م', 'e' } };
+
+        //and a vial to hold minimum fitness for this run
+        public static double MinFitness = double.MaxValue;//initializes to max to make sure min is found after processing
+
+        //holder of this gene layout of the keyboard
+        public char[,] layoutMatrix = new char[LayoutRowCount, LayoutColumnCount];
+
+        //holder of this gene Distance Matrix
+        private double[,] distanceMatrix = new double[N, N];
+
+        //holder of fitness value for this gene
+        public double Fitness = double.MaxValue;//initializes to max to make sure min is found after processing
+
+        //the evaluation function
+        public void ComputeFitness()
+        {
+            //first we need to know the distance matrix for this layout
+            ComputeDistances();
+
+            double timeSum;
+            Fitness = 0;//reset total
+            // then is the time sum of product of flow by log2 distance+1 for each key as in the used formula
+            // using j because the flow matrix is using columns as starter of the digraph
+            for (int j = 0; j < N; j++)
+            {
+                timeSum = 0;
+                for (int i = 0; i < N; i++)
+                {
+                    timeSum += FlowMatrix[i, j] * Math.Log((distanceMatrix[i, j] + 1), 2);
+                }
+
+                // then using the actual usage ratio of character keys each by that time sum
+                Fitness += KeyProbabilityArray[j] * timeSum;
+            }
+        }
+
+        //the creator of comparable distances matrix
+        private void ComputeDistances()
+        {
+            // using formula to calculate distances matrix for the gene but in least calculations just to make compare viable
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    //same key has zero distance
+                    if (i == j)
+                    {
+                        distanceMatrix[i, j] = 0;
+                    }
+                    else //different keys distances but for compare we don't need to find square root (the exact value) to reduce computer instructions
+                    {
+                        Point firstKeyPosition = FindLetterPosition(KeyArrangedByProbabilityArray[i]);
+                        Point secondKeyPosition = FindLetterPosition(KeyArrangedByProbabilityArray[j]);
+
+                        distanceMatrix[i, j] = (secondKeyPosition - firstKeyPosition).LengthSquared;
+                    }
+                }
+            }
+        }
+
+        //helper method to get position of given letter in the layout of this gene
+        private Point FindLetterPosition(char letter)
+        {
+            // search for position of key letter inside this layout
+            for (int i = 0; i < LayoutRowCount; i++)
+            {
+                for (int j = 0; j < LayoutColumnCount; j++)
+                {
+                    //is it the key we want
+                    if (letter == layoutMatrix[i, j])
+                    {
+                        return new Point(i, j);
+                    }
+                }
+            }
+            return new Point(0, 0);//default if not found
+        }
+
+        //this mutates current gene when called
+        public void Mutate()
+        {
+            int delta = 0;//to keep track of the number of mutations per child
+            char temp;//to hold value while swapping
+            Point firstSwapPosition;//position of key to swap
+            Point secondSwapPosition;//and the one to swap with
+
+            foreach (var key in KeyArrangedByProbabilityArray)//because we only want to change these keys, the others are fixed, for example space and backspace are fixed
+            {
+                if (delta < MaxMutations)
+                {
+                    //mutate at random
+                    if (Random.NextDouble() < MutationRate)
+                    {
+                        //find swap key in this gene
+                        firstSwapPosition = FindLetterPosition(key);
+
+                        //choose key to swap with at random and get its place in this gene
+                        secondSwapPosition = FindLetterPosition(chooseOtherKeyRandomly(key));
+
+                        //do the swapping
+                        temp = key;
+                        layoutMatrix[(int)firstSwapPosition.X, (int)firstSwapPosition.Y] = layoutMatrix[(int)secondSwapPosition.X, (int)secondSwapPosition.Y];
+                        layoutMatrix[(int)secondSwapPosition.X, (int)secondSwapPosition.Y] = temp;
+
+                        //add one to delta because we mutated one key position
+                        delta += 1;
+                    }
+                }
+            }
+        }
+
+        //helper method to choose second key of swap and make sure it's a viable choice
+        private char chooseOtherKeyRandomly(char originalKey)
+        {
+            //initialize as same to condition the upcoming loop
+            char otherKey = originalKey;
+
+            //make sure they are different before exiting the method
+            while (otherKey == originalKey)
+            {
+                //choose viable other randomly
+                int randomPositionOfViableOther = Random.Next(KeyArrangedByProbabilityArray.Length);
+                otherKey = KeyArrangedByProbabilityArray[randomPositionOfViableOther];
+            }
+            return otherKey;
+        }
+
+        //constructor
+        public KeyboardGene(char[,] baseLayout)
+        {
+            for (int i = 0; i < LayoutRowCount; i++)
+            {
+                for (int j = 0; j < LayoutColumnCount; j++)
+                {
+                    layoutMatrix[i, j] = baseLayout[i, j];
+                }
+            }
+        }
+    }
+}
